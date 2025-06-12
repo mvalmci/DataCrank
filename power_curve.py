75cc76647fd5168535accb680e0cb9ded4123c14
mvalmci <m.valenta@mci4me.at>
2025-06-12 16:01:05 +0200

    added Streamlit, Pandas, Numpy and main.py, person.py,power_curv.py, pictures from rider, riders_db.json, csv data

---
+++ power_curve.py
@@ -0,0 +1,4 @@
+#power_curve generieren und plotten
+#müde power curve z.B.: 1500kJ verbraucht
+#sehr müde power curve z.B.: 3000kJ verbraucht
+#über gesamten Datensatz drübergehen und Energieverbrauch in Json hinzufügen
---
+++ read_rider_data.py
@@ -0,0 +1,9 @@
+def load_rider():
+
+    return rider_data
+
+def find_rider_data_by_name(suchstring):
+
+    return 
+
+
